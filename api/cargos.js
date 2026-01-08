60d93f7df6d76bd2b610e7ff7e5a3e85505ed415
Matheussousa282 <matheusaraujo2509@outlook.com>
2026-01-08 08:55:19 -0300

    Add files via upload

--- api/cargos.js
+++ api/cargos.js
@@ -1,7 +1,7 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL_RH,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
 
