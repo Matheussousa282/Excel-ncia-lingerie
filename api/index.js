a5ab40866a3f59dc4620ea28c97e0bd1db8b1e53
Matheussousa282 <matheusaraujo2509@outlook.com>
2026-01-09 09:04:36 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL_RH,
   ssl: { rejectUnauthorized: false },
 });
 
@@ -53,3 +53,4 @@ export default async function handler(req, res) {
     res.status(500).send("Erro ao buscar candidatos: " + err.message);
   }
 }
+
